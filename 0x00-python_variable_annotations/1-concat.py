095d3a2566cd911da61b0d4eeb1918f4fea29a8c
Terer <terermike3@gmail.com>
2023-04-28 21:10:32 +0000

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""type-annotated function concat that takes a string str1 and a 
-string str2 as arguments and returns a concatenated string"""
+"""type-annotated function concat that takes a string str1 and
+a string str2 as arguments and returns a concatenated string"""
 
 
-def concat(str1: string, str2: string) -> string:
+def concat(str1: str, str2: str) -> str:
     """Concatenates 2 strings"""
-    return f"str1 + str2"
+    return str1 + str2
