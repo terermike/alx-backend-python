b989de57160561b04b0e5f538b4ffa7323a31b6d
Terer <terermike3@gmail.com>
2023-04-28 22:38:03 +0000

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,6 +6,6 @@ int/float v and should be annotated as a float."""
 from typing import List, Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple"""
     return (k, v * v)
