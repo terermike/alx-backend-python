cdbd6c5b4ddd1de982c536eb0d5f98fdbf3f9a82
Terer <terermike3@gmail.com>
2023-05-03 22:26:51 +0000

    4-tasks.py

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 4's module.
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Executes task_wait_random n times.
+    """
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    wait_times = await asyncio.gather(*coroutines)
+    return sorted(wait_times)
